118e88317efb2ef078efc0a290d04c27ca7e27b2
Peter Mayr <at.peter.mayr@posteo.de>
2016-10-02 20:44:06 +0200

    initial setup

---
+++ WWW-Lobid-Organisation/lib/WWW/Lobid.pm
@@ -0,0 +1,10 @@
+package WWW::Lobid;
+
+use strict;
+use warnings;
+
+use Moo;
+
+has api_url => ( is=> ro, default=> 'https://lobid.org/')
+
+1;
---
+++ WWW-Lobid-Organisation/lib/WWW/Lobid/Organisation.pm
@@ -0,0 +1,12 @@
+package WWW::Lobid::Organisation;
+
+use strict;
+use warnings;
+
+use Moo;
+
+extends 'WWW::Lobid';
+
+# ABSTRACT: interface to the lobid-Organisations API
+
+1;
