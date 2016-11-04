1463d3cfef33b27ae34a5bf50c1d65bd15f2b0a5
Peter Mayr <at.peter.mayr@posteo.de>
2016-11-04 21:04:28 +0100

    get Address, set flag 'found' true|false, added test

--- WebService-Lobid-Organisation/lib/WebService/Lobid/Organisation.pm
+++ WebService-Lobid-Organisation/lib/WebService/Lobid/Organisation.pm
@@ -40,50 +40,55 @@ use Try::Tiny;
 
 extends 'WebService::Lobid';
 
-has isil => ( is => 'rw', predicate=>1, required => 1);
-has name => (is => 'rw', predicate=>1);
-has url => (is => 'rw', predicate => 1);
-has wikipedia => (is => 'rw', predicate => 1);
-has email => (is => 'rw', predicate => 1);
-has has_multiple_emails => ( is => 'rw', default => 0);
-has long => (is => 'rw', predicate => 1);
-has lat => (is => 'rw', predicate => 1);
+has isil => ( is => 'rw', predicate => 1, required => 1 );
+has name => ( is => 'rw', predicate => 1 );
+has url  => ( is => 'rw', predicate => 1 );
+has wikipedia           => ( is => 'rw', predicate => 1 );
+has countryName         => ( is => 'rw', predicate => 1 );
+has locality            => ( is => 'rw', predicate => 1 );
+has postalCode          => ( is => 'rw', predicate => 1 );
+has streetAddress       => ( is => 'rw', predicate => 1 );
+has email               => ( is => 'rw', predicate => 1 );
+has has_multiple_emails => ( is => 'rw', default   => 0 );
+has long                => ( is => 'rw', predicate => 1 );
+has lat                 => ( is => 'rw', predicate => 1 );
+has found               => ( is => 'rw', default   => 'false' );
 
 has log => (
     is      => 'ro',
     default => sub { Log::Any->get_logger },
 );
 
-
-
-sub BUILD{
+sub BUILD {
     my $self = shift;
-    
-    my $query_string = undef;
-    my $response = undef;
-    my $json_result  = undef;
-    my $result_ref = undef;
-    my %data = ();
-    my $email = undef;
-    my $uri = sprintf( "%s%s/%s",
-		       $self->api_url, "organisation", $self->isil);
-    
-    $query_string = sprintf( "%s%s?id=%s&format=full",
-			     $self->api_url, "organisation", $self->isil);
 
-    $self->log->infof("URL: %s", $query_string);
+    my $query_string  = undef;
+    my $response      = undef;
+    my $json_result   = undef;
+    my $result_ref    = undef;
+    my $no_of_results = undef;
+    my %data          = ();
+    my $email         = undef;
+    my $uri = sprintf( "%s%s/%s", $self->api_url, "organisation", $self->isil );
+
+    $query_string = sprintf( "%s%s?id=%s&format=full",
+        $self->api_url, "organisation", $self->isil );
+
+    $self->log->infof( "URL: %s", $query_string );
     $response = HTTP::Tiny->new->get($query_string);
 
-    if ($response->{success}) {
-      $json_result = $response->{content};
+    if ( $response->{success} ) {
+        $json_result = $response->{content};
     }
     else {
-      $self->log->errorf("Problem accessing the API: %s!",
-			 $response->{status});
-      $result_ref->{success} = 0;
-      $result_ref->{error_msg} = $response->{status};
-      return $result_ref;
+        $self->log->errorf( "Problem accessing the API: %s!",
+            $response->{status} );
+        $result_ref->{success}   = 0;
+        $result_ref->{error_msg} = $response->{status};
+        return $result_ref;
     }
+
+    $self->log->debugf( "Got JSON Result: %s", $json_result );
 
     try {
         $result_ref = decode_json($json_result);
@@ -92,36 +97,58 @@ sub BUILD{
         $self->log->errorf( "Decoding of response '%s' failed: %s",
             $json_result, $_ );
     };
-    foreach my $g (@{$result_ref->[1]->{'@graph'}}){
-	$data{$g->{'@id'}} = $g;
+
+    if ( $result_ref->[0]->{'http://sindice.com/vocab/search#totalResults'} ) {
+        $no_of_results =
+          $result_ref->[0]->{'http://sindice.com/vocab/search#totalResults'};
+        $self->log->infof( "Got %d results", $no_of_results );
+        $self->found("true") if ( $no_of_results == 1 );
     }
-    if (exists($data{$uri})){
-     
-    	$self->name($data{$uri}->{name}) if ($data{$uri}->{name});
-    	$self->url($data{$uri}->{url}) if ($data{$uri}->{url});
-    	$self->wikipedia($data{$uri}->{wikipedia}) if ($data{$uri}->{wikipedia});
-    	if ($data{$uri}->{email}){
-    	    $email = $data{$uri}->{email};
-    	    if (ref($email) eq 'ARRAY'){ # multiple E-Mail Adresses
-    		$self->has_multiple_emails(1);
-    		for (my $i=0; $i < scalar(@{$email}); $i++){
-    		    $email->[$i] =~ s/^mailto://;
-    		}
-    	    }else{
-		
-    		$email =~ s/^mailto://;
-		
-    	    }
-    	    $self->email($email);
-    	}
-	if ($data{$uri}->{location}){
-	    $self->lat($data{$data{$uri}->{location}}->{lat});
-	    $self->long($data{$data{$uri}->{location}}->{long});
-	}
+
+    foreach my $g ( @{ $result_ref->[1]->{'@graph'} } ) {
+        $data{ $g->{'@id'} } = $g;
+    }
+    if ( exists( $data{$uri} ) ) {
+        $self->log->debugf("Data %s",$data{$uri}); 
+        $self->name( $data{$uri}->{name} ) if ( $data{$uri}->{name} );
+        $self->url( $data{$uri}->{url} )   if ( $data{$uri}->{url} );
+        $self->wikipedia( $data{$uri}->{wikipedia} )
+          if ( $data{$uri}->{wikipedia} );
+        if ( $data{$uri}->{email} ) {
+            $email = $data{$uri}->{email};
+            if ( ref($email) eq 'ARRAY' ) {    # multiple E-Mail Adresses
+                $self->has_multiple_emails(1);
+                for ( my $i = 0 ; $i < scalar( @{$email} ) ; $i++ ) {
+                    $email->[$i] =~ s/^mailto://;
+                }
+            }
+            else {
+
+                $email =~ s/^mailto://;
+
+            }
+            $self->email($email);
+        }
+        if ( $data{$uri}->{location} ) {
+            $self->lat( $data{ $data{$uri}->{location} }->{lat} );
+            $self->long( $data{ $data{$uri}->{location} }->{long} );
+        }
+        if ( $data{$uri}->{address} ) {
+
+            $self->countryName( $data{ $data{$uri}->{address} }->{countryName} )
+              if ( $data{ $data{$uri}->{address} }->{countryName} );
+            $self->locality( $data{ $data{$uri}->{address} }->{locality} )
+              if ( $data{ $data{$uri}->{address} }->{locality} );
+            $self->postalCode( $data{ $data{$uri}->{address} }->{postalCode} )
+              if ( $data{ $data{$uri}->{address} }->{postalCode} );
+            $self->streetAddress(
+                $data{ $data{$uri}->{address} }->{streetAddress} )
+              if ( $data{ $data{$uri}->{address} }->{streetAddress} );
+
+        }
 
     }
 
-    
 }
 
 1;
--- WebService-Lobid-Organisation/t/01.organisation.t
+++ WebService-Lobid-Organisation/t/01.organisation.t
@@ -1,11 +1,10 @@
-use Test::More tests => 5;
+use Test::More tests => 7;
 
 use strict;
 use warnings;
 use utf8;
 
 use Encode;
-use Log::Any::Adapter ('Stderr');
 
 BEGIN {
 use_ok( 'WebService::Lobid::Organisation' );
@@ -14,6 +13,10 @@ use_ok( 'WebService::Lobid::Organisation' );
 my $O = WebService::Lobid::Organisation->new(isil=>'DE-380');
 
 is($O->api_url,'http://lobid.org/', "API-URL found");
+is($O->found,'true', "ISIL found");
 is($O->name,'Stadtbibliothek Köln',"Name found");
 is($O->url,'http://www.stbib-koeln.de/', "URL found");
 is($O->wikipedia,'http://de.wikipedia.org/wiki/Stadtbibliothek_Köln',"Wikipedia Page found");
+
+$O = WebService::Lobid::Organisation->new(isil=>'foo');
+is($O->found,'false', "ISIL 'foo' not found");
